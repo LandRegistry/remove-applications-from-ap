527f301925ab8c428c054ec69ff111714fe8a41e
Chris Armstrong <mail@chrisarmstrong.io>
2021-01-19 14:37:22 +0000

    Code formatting

--- app/views/v1/_routes.js
+++ app/views/v1/_routes.js
@@ -3,16 +3,20 @@ const router = new express.Router()
 
 // Search applications from default session data file and render details or error
 router.post('/find', (req, res) => {
-  const search = req.session.data['find'].replace(/ /g,'').toUpperCase()
+  const search = req.session.data['find'].replace(/ /g, '').toUpperCase()
   const application = req.session.data['applications'].filter(application => application.abr === search)
 
   res.locals.application = application[0]
 
   if (search.length == 0) {
-    res.render('v1/find', { error: 'Enter an Application Barcode Reference' })
+    res.render('v1/find', {
+      error: 'Enter an Application Barcode Reference'
+    })
   } else {
     if (application == false) {
-      res.render('v1/find', { error: 'Check the Application Barcode Reference is correct' })
+      res.render('v1/find', {
+        error: 'Check the Application Barcode Reference is correct'
+      })
     } else {
       res.render('v1/result')
     }
