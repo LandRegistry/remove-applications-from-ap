31541be08cda1ab8c094cd27ccdff56379e8020b
Chris Armstrong <mail@chrisarmstrong.io>
2021-02-24 16:25:18 +0000

    Couple more tweaks from EMily

--- app/views/v2/_routes.js
+++ app/views/v2/_routes.js
@@ -15,7 +15,7 @@ router.post('/find', (req, res) => {
   } else {
     if (application == false) {
       res.render('v2/find', {
-        error: 'You have entered an invalid ABR or the application is not in Application Processing'
+        error: 'You have entered an invalid ABR or the application is not in Application Processing, check the current ESRV queue in General Enquiries'
       })
     } else {
       res.render('v2/result')
